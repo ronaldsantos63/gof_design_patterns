d4c67d081cf64fba1f7f46f99f9ccf195a0967d7
Ronald Santos <ronaldsantos63@gmail.com>
2025-10-25 23:17:12 -0300

    refactor(build-logic,core): add @Suppress("unused") to plugins and modules
    - Add `@Suppress("unused")` to convention plugins in `build-logic`
    - Add `@Suppress("unused")` to `DataModule` in `core-data`

--- build-logic/src/main/kotlin/pocketshop/buildlogic/KotlinLibraryConventionPlugin.kt
+++ build-logic/src/main/kotlin/pocketshop/buildlogic/KotlinLibraryConventionPlugin.kt
@@ -6,6 +6,7 @@ import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+@Suppress("unused")
 class KotlinLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
